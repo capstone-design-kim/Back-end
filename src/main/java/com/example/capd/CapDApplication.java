91e51eda9e93eab2ad5ca1e08963a688194d88d2
jwj9127 <jwj9127@naver.com>
2024-03-06 23:46:00 +0900

    크롤링 추가

--- src/main/java/com/example/capd/CapDApplication.java
+++ src/main/java/com/example/capd/CapDApplication.java
@@ -2,8 +2,10 @@ package com.example.capd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling   //서버켜자마자 크롤링되는거 싫으면 주석처리하세요
 public class CapDApplication {
 
     public static void main(String[] args) {
